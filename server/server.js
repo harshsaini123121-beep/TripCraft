6cc0ec97d9b0369d5ecc529cdf48d1c37fca1b69
hackharsh2005 <harshsaini123121@gmail.com>
2026-06-14 11:29:54 +0530

    Serve client build files from server in production

--- server/server.js
+++ server/server.js
@@ -20,6 +20,18 @@ app.use('/api/trip', require('./routes/tripRoutes'));
 
 app.get('/api/health', (req, res) => res.json({ status: 'ok', message: 'TripCraft API running 🚀' }));
 
+// Serve frontend build static files in production
+const fs = require('fs');
+const path = require('path');
+const distPath = path.join(__dirname, '../client/dist');
+if (fs.existsSync(distPath)) {
+  app.use(express.static(distPath));
+  app.get('*', (req, res, next) => {
+    if (req.path.startsWith('/api')) return next();
+    res.sendFile(path.join(distPath, 'index.html'));
+  });
+}
+
 app.use((err, req, res, next) => {
   console.error(err.stack);
   res.status(500).json({ message: 'Something went wrong!' });
